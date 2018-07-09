e82e95cba6e5858713b75fda6f7fa4a6238f883c
Rondineli <raraujo04@yahoo.com.br>
2018-07-09 16:33:52 +0100

    flake8

--- app.py
+++ app.py
@@ -4,8 +4,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    message = 'Hello, World! version: {}\n'.format(os.environ.get("VERSION", "no version created"))
-    message += "Thats it, let's move on..."
+    message = 'Hello, World! version: {}\n'.format(
+        os.environ.get(
+            "VERSION",
+            "no version created"
+        )
+    )
+    message += "Thats it!"
     return message
