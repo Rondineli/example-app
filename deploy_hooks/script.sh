6f19555a30fabd16bea518b97c105c9cef851062
Rondineli <raraujo04@yahoo.com.br>
2018-07-09 17:38:51 +0100

    Added deployment hoooks

---
+++ deploy_hooks/script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+BASEDIR=$(dirname "$0")
+
+if [ -z ${CIRCLE_TAG} ]; then
+	VERSION_APP=${CIRCLE_TAG}
+	K8S_ENDPOINT=${K8S_ENDPOINT_STAG}
+	K8S_TOKEN=${K8S_TOKEN_STAG}
+else
+	VERSION_APP=${CIRCLE_SHA1}
+fi
+
+echo "installing kubectl ..."
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/kubernetes.list 
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+### Kube Config role
+sed -i "s/K8S_ENDPOINT_REPLACE/${K8S_ENDPOINT}/g" ./${BASEDIR}/kube_config
+sed -i "s/TOKEN_REPLACE/${K8S_TOKEN}/g" ./${BASEDIR}/kube_config
+
+### Deployment 
+sed -i "s/VERSION_REPLACE/${VERSION_APP}/g" ./${BASEDIR}/deployment.yaml
+sed -i "s/ECR_ENDPOINT_URL/${K8S_ENDPOINT}/g" ./${BASEDIR}/deployment.yaml
+
+export KUBECONFIG=./${BASEDIR}/kube_config
+
+kubectl apply -f ./${BASEDIR}/deployment.yaml
